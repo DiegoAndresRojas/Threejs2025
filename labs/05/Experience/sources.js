0ea2bfef2863101b86c7022b0d038a8c74761f3a
d4ndres <d4ndres@gmail.com>
2025-11-23 03:15:51 -0600

    fox in arquitecture

---
+++ labs/05/Experience/sources.js
@@ -0,0 +1,32 @@
+
+
+
+export default [
+    {
+        name: 'environmentMapTexture',
+        type: 'cubeTexture',
+        path: [
+            '/assets/textures/grass.png',
+            '/assets/textures/grass.png',
+            '/assets/textures/grass.png',
+            '/assets/textures/grass.png',
+            '/assets/textures/grass.png',
+            '/assets/textures/grass.png',
+        ],
+    },
+    {
+        name: 'grassColorTexture',
+        type: 'texture',
+        path: '/assets/textures/grass.png',
+    },
+    {
+        name: 'grassNormalTexture',
+        type: 'texture',
+        path: '/assets/textures/grass.png',
+    },
+    {
+        name: 'foxModel',
+        type: 'gltfModel',
+        path: '/assets/models/Fox/glTF/Fox.gltf'
+    }
+]
